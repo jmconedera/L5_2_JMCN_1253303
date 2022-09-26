0e9aa584d695f8c87028148ed32431f31fb6e5a6
MCONEDERA <MCONEDERA@GLC2539>
2022-09-26 13:39:43 -0600

    Add project files.

---
+++ L5_2_JMCN_1253303/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero =0
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Numero Entero");
+numeroEntero = Console.Read();
+if(numeroEntero==1)
+{
+ Console.WriteLine("Lunes"); 
+}else if(numeroEntero==2)
+{
+Console.WriteLine("Martes");
+}else if (numeroEntero == 3)
+{
+ Console.WriteLine("Miercoles");
+}else if (numeroEntero == 4)
+{
+Console.WriteLine("Jueves")
+}else if (numeroEntero == 5)
+{
+Console.WriteLine("Viernes")
+}elfe if (numeroEntero == 6)
+
